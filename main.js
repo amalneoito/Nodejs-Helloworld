bf2e23ee0bd092482358dc8cd61e600cffc4ee16
amalneoito <85620265+amalneoito@users.noreply.github.com>
2022-05-09 15:45:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello friends\n');
+   response.end('Hello didit\n');
 }).listen(8081);
 
 // Console will print the message
